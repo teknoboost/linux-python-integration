a0022c7718aa3be827afa31b028fa1ff72402dad
Hemant Gangwar <34183672+whytedork@users.noreply.github.com>
2020-10-11 09:54:31 +0530

    updated docker operations

---
+++ docker_operations_all.py
@@ -0,0 +1,41 @@
+from os import system as sys
+
+#option18 Docker Operations
+def what_docker(x):
+	if x == 1:
+		sys('sudo docker images')
+		print()
+
+	elif x == 2:
+		os_name = input("Enter os_name : ")
+		image_name = input("Enter image_name : ")
+		sys("sudo docker run -dit --name {} {}".format(os_name,image_name))
+		print()
+
+	elif x == 3:
+		sys('sudo docker ps -a')
+		print()
+
+	elif x == 4:
+		os_name = input("Enter os_name which you want to stop : ")
+		sys('sudo docker rm -f {}'.format(os_name))
+		print()
+
+	elif x == 5:
+		image_name = input("Enter image_name : ")
+		tag = input("Enter tag_name : ")
+		sys('sudo docker pull {}:{}'.format(image_name,tag))
+		print()
+
+	elif x == 6:
+		sys('sudo docker network ls')
+		print()
+
+	elif x == 7:
+		os_name = input("Enter os_name : ")
+		sys('sudo docker exec -it {} bash'.format(os_name))
+		print()
+
+	else :
+		print("Invalid input !!!")
+	return
