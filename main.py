386d50143765af9dbfefd1aa352f51d8df25b570
JyotirmayaV <jyotirmayavasaniwal@gmail.com>
2020-10-13 13:49:43 +0530

    created new function for displaying the options

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import getpass
 from termcolor import colored
 from python_operations import what_py
 from docker_operations_all import what_docker
+from all_operations import all_operations_display
 import smtplib
 
 #code for banner and designs
@@ -27,28 +28,7 @@ else :
 #menu-bar
 if x == password:
 	while True:
-		print(colored("""Enter 1  : to login as root user
-Enter 2  : to see date & time 
-Enter 3  : to open calculator
-Enter 4  : to create new user
-Enter 5  : to install new software or tool
-Enter 6  : to open new terminal 
-Enter 7  : to open text-editor
-Enter 8  : to update whole system
-Enter 9  : to Python operations
-Enter 10 : to scan website using nmap
-Enter 11 : to check ip address
-Enter 12 : to ping
-Enter 13 : to configure web server
-Enter 14 : to create banners
-Enter 15 : to run any command
-Enter 16 : to launch browser
-Enter 17 : to search location of installed software/tool
-Enter 18 : to Docker Operations
-Enter 19 : to open website
-Enter 20 : to send an email
-Enter 21 : to exit
-	""","blue"))
+		all_operations_display()
 		choice = int(input("Enter Your Choice : "))
 
 		if(choice == 1):
