9fd6df8e3744c0502e214b94860edb1890f9adef
Akansh Agarwal <55538907+akansh028@users.noreply.github.com>
2020-10-13 13:35:59 +0530

    Added firefox functionality

--- main.py
+++ main.py
@@ -45,7 +45,8 @@ Enter 17 : to search location of installed software/tool
 Enter 18 : to Docker Operations
 Enter 19 : to open website
 Enter 20 : to send an email
-Enter 21 : to exit
+Enter 21 : to open firefox
+Enter 22 : to exit
 	""","blue"))
 		choice = int(input("Enter Your Choice : "))
 
@@ -166,8 +167,12 @@ Enter 21 : to exit
 			print("LogIn Successfull")
 			server.sendmail(sender_email,rec_email,message)
 			print("Your Email has been sent to : ",rec_email)
-
+		
 		elif choice == 21:
+			sys("firefox")
+			print()
+			
+		elif choice == 22:
 			sys("exit")
 			print(colored("Thanks For Using","red"))
 			break
